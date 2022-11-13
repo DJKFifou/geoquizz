fe71e2ca61e8d0958cb56cda18deba26b826056c
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2022-11-13 04:14:17 +0100

    Tentetive redirection liens

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   root "home#home"
 
-  get '/quizz' => 'quizz#index'
+  get '/quizz-hard' => 'quizz-hard#index'
   get '/quizz-easy' => 'quizz-easy#index'
   get '/quizz-medium' => 'quizz-medium#index'
 end
