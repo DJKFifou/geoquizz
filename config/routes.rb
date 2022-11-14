1f9bd03d32b290c274eb90411a83b79aa83d9562
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2022-11-14 23:23:05 +0100

    redirection liens

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   root "home#home"
 
-  get '/quizzeasy' => 'quizz#index'
+  get '/quizzeasy' => 'quizzeasy#index'
   get '/quizzmedium' => 'quizz#index'
-  get '/quizzhard' => 'quizz#index'
+  get '/quizzhard' => 'quizzhard#index'
 end
