a847895c7188127a8f4c69a38c0b8dcd66b974f8
Maxime <lust.maxime@outlook.fr>
2022-11-15 16:14:26 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :questions
   root "home#home"
 
-  get '/quizzeasy' => 'quizz#index'
+  get '/quizz' => 'quizz#index'
   get '/quizzmedium' => 'quizz#index'
   get '/quizzhard' => 'quizz#index'
 end
