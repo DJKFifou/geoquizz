40fa68eadb87d1b8ad3fb9caee96e040f7f39d4b
Maxime <lust.maxime@outlook.fr>
2022-12-28 18:57:53 +0100

    test redirection questionshard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :questions
+  resources :questionshard
   root "home#home"
 
   get '/quizz' => 'quizz#index'
-  get '/quizzmedium' => 'quizz#index'
-  get '/quizzhard' => 'quizz#index'
+  get '/quizzmedium' => 'quizz#medium'
+  get '/quizzhard' => 'quizz#hard'
 end
