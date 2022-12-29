80a512875cbb287b58f59b3c616c215b47b3a9e6
Maxime <lust.maxime@outlook.fr>
2022-12-29 16:22:26 +0100

    remove hard files

--- app/controllers/quizz_controller.rb
+++ app/controllers/quizz_controller.rb
@@ -8,13 +8,4 @@ class QuizzController < ApplicationController
     @answers << { label: @question.wrong_answer_3, correct: false }
     @answers.shuffle!
   end
-  def hard
-    @questionhard = Questionhard.all.sample
-    @answershard = []
-    @answershard << { label: @questionhard.correct_answer, correct: true }
-    @answershard << { label: @questionhard.wrong_answer_1, correct: false }
-    @answershard << { label: @questionhard.wrong_answer_2, correct: false }
-    @answershard << { label: @questionhard.wrong_answer_3, correct: false }
-    @answershard.shuffle!
-  end
 end
