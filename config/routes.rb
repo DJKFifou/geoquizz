b669f5efaabb13260cd8d5bd5582769880febea9
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2023-01-01 17:13:53 +0100

    test liens pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :questions
+  resources :questionshard
   root "home#home"
 
   get '/quizz' => 'quizz#index'
-  get '/quizz' => 'quizz#hard'
-  get '/quizz' => 'quizz#medium'
+  get '/quizzmedium' => 'quizzmedium#index'
+  get '/quizzhard' => 'quizzhard#index'
 end
