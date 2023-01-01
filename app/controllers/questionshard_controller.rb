a655f4b0cd415fa796da31cc1b0cb4d1bc828400
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2023-01-01 18:00:54 +0100

    test page hard

--- app/controllers/questionshard_controller.rb
+++ app/controllers/questionshard_controller.rb
@@ -1,5 +1,5 @@
 class QuestionshardController < ApplicationController
-  before_action :set_question, only: %i[ show edit update destroy ]
+  before_action :set_questionhard, only: %i[ show edit update destroy ]
 
   # GET /questions or /questions.json
   def hard
--- app/controllers/quizzhard_controller.rb
+++ app/controllers/quizzhard_controller.rb
@@ -1,11 +1,11 @@
 class QuizzhardController < ApplicationController
   def hard
-    @question = Question.all.sample
+    @questionhard = Questionhard.all.sample
     @answers = []
-    @answers << { label: @question.correct_answer, correct: true }
-    @answers << { label: @question.wrong_answer_1, correct: false }
-    @answers << { label: @question.wrong_answer_2, correct: false }
-    @answers << { label: @question.wrong_answer_3, correct: false }
+    @answers << { label: @questionhard.correct_answer, correct: true }
+    @answers << { label: @questionhard.wrong_answer_1, correct: false }
+    @answers << { label: @questionhard.wrong_answer_2, correct: false }
+    @answers << { label: @questionhard.wrong_answer_3, correct: false }
     @answers.shuffle!
   end
 end
