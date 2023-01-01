a394adad7382e2e6aa335d48f62f876afe6fc00f
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2023-01-02 00:11:41 +0100

    test page hard

--- app/controllers/quizzhard_controller.rb
+++ app/controllers/quizzhard_controller.rb
@@ -1,11 +1,11 @@
-class QuizzhardController < ApplicationController
+class QuizzController < ApplicationController
   def index
-    @questionhard = Questionhard.all.sample
+    @question = Question.all.sample
     @answers = []
-    @answers << { label: @questionhard.correct_answer, correct: true }
-    @answers << { label: @questionhard.wrong_answer_1, correct: false }
-    @answers << { label: @questionhard.wrong_answer_2, correct: false }
-    @answers << { label: @questionhard.wrong_answer_3, correct: false }
+    @answers << { label: @question.correct_answer, correct: true }
+    @answers << { label: @question.wrong_answer_1, correct: false }
+    @answers << { label: @question.wrong_answer_2, correct: false }
+    @answers << { label: @question.wrong_answer_3, correct: false }
     @answers.shuffle!
   end
 end
