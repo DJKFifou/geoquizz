446a5cb958e511e67a2512a879666eb714b2e545
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2023-01-02 02:37:31 +0100

    test page hard

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,4 @@
-class QuestionshardController < ApplicationController
+class QuestionsController < ApplicationController
   before_action :set_question, only: %i[ show edit update destroy ]
 
   # GET /questions or /questions.json
